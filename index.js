ac0a8ae1948eae490745420afbf5c1127f133d20
Hannah Maeve Andersen <hannahmaeveandersen@gmail.com>
2024-04-16 15:09:49 +0000

    fixed relative path issue

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
